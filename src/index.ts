e4d44d8bc1c9b334f49cd65394010ae896394faa
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2023-12-27 14:36:27 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hola')
+import CliInputParser from './cliInputParser'
+import OperationConverter from './operationConverter'
+import StockMarketInvestment from './stockMarketInvestment'
+
+process.stdin.setEncoding('utf8')
+
+console.log('>')
+
+process.stdin.on('data', (input) => {
+  try {
+    const jsonOperationCollectionArray: any[][] = new CliInputParser(input.toString()).parse()
+    const operationsCollectionArray = new OperationConverter(jsonOperationCollectionArray).process()
+    const stockMarketInvestment = new StockMarketInvestment(operationsCollectionArray)
+
+    console.log(
+      stockMarketInvestment.getTaxes()
+    )
+  } catch (e) {
+    console.error('An error has occurred, please try again')
+  }
+})
