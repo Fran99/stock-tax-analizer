e3ea919f770a85fd8d08d3f30b6622ea855cb37c
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2023-12-27 14:36:15 -0300

    Create cliInputParser.ts

---
+++ src/cliInputParser.ts
@@ -0,0 +1,7 @@
+export default class CliInputParser {
+  constructor (public data: string) {}
+  parse (): any[] {
+    const parsedInput = this.data.trim().split('\n')
+    return parsedInput.filter((item) => item.length > 0).map((item) => JSON.parse(item))
+  }
+}
