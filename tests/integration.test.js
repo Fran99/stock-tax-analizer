10455d7d9faefe33e01259bceb33d649023dda0f
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2023-12-27 11:26:02 -0300

    First commit

---
+++ tests/integration.test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai_1 = require("chai");
+describe('Integration', () => {
+    it('Should work', () => {
+        (0, chai_1.expect)(1).to.equals(1);
+    });
+});
---
+++ tests/integration.test.ts
@@ -0,0 +1,7 @@
+import {expect} from "chai";
+
+describe('Integration', () => {
+    it('Should work', () => {
+        expect(1).to.equals(1);
+    })
+})
