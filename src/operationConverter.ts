d608beb8832f662563b8ee01bc285dd2972bfa0b
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2023-12-27 14:36:38 -0300

    Create operationConverter.ts

---
+++ src/operationConverter.ts
@@ -0,0 +1,30 @@
+import { Operation } from './operation'
+
+/**
+ * Transforms a plain JS array into an array of operation collections.
+ *
+ * plainOperationCollectionArray
+ * [
+ *  [{...}, {...}, {...}] -> each line is an independent simulation, a collection of operations
+ *  [{...}, {...}, {...}] -> ...
+ *  [{...}, {...}, {...}] -> ...
+ * ]
+ *
+ * plainOperationCollection
+ * [{...}, {...}, {...}]
+ *
+ * plainOperation
+ * {...}
+ *
+ */
+export default class OperationConverter {
+  constructor (public plainOperationCollectionArray: any[][]) {}
+
+  process (): Operation[][] {
+    return this.plainOperationCollectionArray.map((plainOperationArray) => {
+      return plainOperationArray.map((plainOperation: { operation: 'buy' | 'sell', ['unit-cost']: number, quantity: number }) =>
+        new Operation(plainOperation.operation, plainOperation['unit-cost'], plainOperation.quantity)
+      )
+    })
+  }
+}
