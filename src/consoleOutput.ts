77e636e9837dcb3793c8acc356a203139009f1da
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2023-12-28 10:06:35 -0300

    Update consoleOutput.ts

--- src/consoleOutput.ts
+++ src/consoleOutput.ts
@@ -1,6 +1,8 @@
-export class ConsoleOutput {
-  constructor (public data: any[]) {}
-  write (): void {
-    console.log(this.data)
+import { type IOutput } from './interfaces/IOutput'
+import { type ResponseDto } from './dtos/responseDto'
+
+export class ConsoleOutput implements IOutput {
+  write (data: ResponseDto[][]): void {
+    console.log(data)
   }
 }
