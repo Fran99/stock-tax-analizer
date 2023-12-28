49b29a396a318ea801105a13334ed51bf74a2db6
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2023-12-28 10:07:21 -0300

    Update stockMarketInvestment.ts

--- src/stockMarketInvestment.ts
+++ src/stockMarketInvestment.ts
@@ -1,12 +1,14 @@
 import { type Operation } from './operation'
 import { TaxCalculator } from './taxCalculator'
+import { type ResponseDto } from './dtos/responseDto'
 
 export class StockMarketInvestment {
   constructor (
     private readonly operationCollectionArray: Operation[][]
   ) {}
 
-  getTaxes (): any[] {
+  getTaxes (): ResponseDto[][] {
+    // Loop over each collection, create a new calculator and execute
     return this.operationCollectionArray.map((operationCollection) => {
       const operationCalculator = new TaxCalculator()
       return operationCalculator.execute(operationCollection)
