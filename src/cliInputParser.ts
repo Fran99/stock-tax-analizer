2cdda84cd9cd516152f3048450b9228e781ab8dd
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2023-12-28 10:06:28 -0300

    Update cliInputParser.ts

--- src/cliInputParser.ts
+++ src/cliInputParser.ts
@@ -1,6 +1,10 @@
-export class CliInputParser {
+import { type OperationDto } from './dtos/operation.dto'
+import { type IParser } from './interfaces/IParser'
+
+export class CliInputParser implements IParser {
   constructor (public data: string) {}
-  parse (): any[] {
+
+  parse (): OperationDto[][] {
     const parsedInput = this.data.trim().split('\n')
     return parsedInput.filter((item) => item.length > 0).map((item) => JSON.parse(item))
   }
