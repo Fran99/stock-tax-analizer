65eff2f5a79283c5c7eac8999f8e7fa8a193233b
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2023-12-28 10:06:51 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@ import { CliInputParser } from './cliInputParser'
 import { OperationConverter } from './operationConverter'
 import { StockMarketInvestment } from './stockMarketInvestment'
 import { ConsoleOutput } from './consoleOutput'
+import { type OperationDto } from './dtos/operation.dto'
+import { type Operation } from './operation'
+import { type ResponseDto } from './dtos/responseDto'
 
 process.stdin.setEncoding('utf8')
 
-console.log('>')
-
 process.stdin.on('data', (input) => {
   try {
-    const jsonOperationCollectionArray: any[][] = new CliInputParser(input.toString()).parse()
-    const operationsCollectionArray = new OperationConverter(jsonOperationCollectionArray).process()
+    const jsonOperationCollectionArray: OperationDto[][] = new CliInputParser(input.toString()).parse()
+    const operationsCollectionArray: Operation[][] = new OperationConverter(jsonOperationCollectionArray).convert()
     const stockMarketInvestment = new StockMarketInvestment(operationsCollectionArray)
-
-    const taxes = stockMarketInvestment.getTaxes();
-    (new ConsoleOutput(taxes)).write()
+    const taxes: ResponseDto[][] = stockMarketInvestment.getTaxes();
+    (new ConsoleOutput()).write(taxes)
   } catch (e) {
     console.error('An error has occurred, please try again')
   }
