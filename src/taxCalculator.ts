b60c2621f1068d00b5aac128c58f4f85232488da
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2023-12-28 10:07:24 -0300

    Update taxCalculator.ts

--- src/taxCalculator.ts
+++ src/taxCalculator.ts
@@ -1,62 +1,57 @@
 import { type Operation } from './operation'
-
-interface Tax {
-  tax: number
-}
+import { type ResponseDto } from './dtos/responseDto'
 
 /**
  * Class in charge of calculate taxes of a collection of operations.
  */
 export class TaxCalculator {
-  private readonly taxes: Tax[] = []
   private losses = 0
   private weightedAverage = 0
   private stocksQty = 0
-  private currentOperation!: Operation
   private readonly taxPercentage = 20 / 100
   private readonly limit = 20000
 
-  execute (operationCollection: Operation[]): Tax[] {
+  execute (operationCollection: Operation[]): ResponseDto[] {
     return operationCollection.map((operation: Operation) => {
-      this.currentOperation = operation
-      if (operation.isBuying()) { return this.buy() }
-      if (operation.unitCost < this.weightedAverage) { return this.sellWithLoss() }
-      if (operation.getOperationAmount() <= this.limit) { return this.sellWithinLimit() }
-      return this.sellWithProfit()
+      if (operation.isBuying()) { return this.buy(operation) }
+      if (operation.unitCost < this.weightedAverage) { return this.sellWithLoss(operation) }
+      if (operation.getOperationAmount() <= this.limit) { return this.sellWithinLimit(operation) }
+      return this.sellWithProfit(operation)
     }).map(amount => {
       return { tax: amount }
     })
   }
 
-  private buy (): number {
+  private buy (operation: Operation): number {
     // Every buy we need to calculate the weighted average price.
     this.weightedAverage = ((this.stocksQty * this.weightedAverage) +
-            (this.currentOperation.qty * this.currentOperation.unitCost)) /
-            (this.stocksQty + this.currentOperation.qty)
+            (operation.qty * operation.unitCost)) /
+            (this.stocksQty + operation.qty)
     this.weightedAverage = parseFloat(this.weightedAverage.toFixed(2))
     // Add stocks to the overall count
-    this.stocksQty += this.currentOperation.qty
+    this.stocksQty += operation.qty
 
     // Buying stocks do not pay taxes
     return 0
   }
 
-  private sellWithProfit (): number {
-    this.stocksQty -= this.currentOperation.qty
-    const profit = ((this.currentOperation.unitCost - this.weightedAverage) * this.currentOperation.qty) - this.losses
-    this.losses -= (this.currentOperation.unitCost - this.weightedAverage) * this.currentOperation.qty
+  private sellWithProfit (operation: Operation): number {
+    this.stocksQty -= operation.qty
+    const profit = ((operation.unitCost - this.weightedAverage) * operation.qty) - this.losses
+    this.losses -= (operation.unitCost - this.weightedAverage) * operation.qty
     if (this.losses < 0) this.losses = 0
+
     return profit > 0 ? profit * this.taxPercentage : 0
   }
 
-  private sellWithLoss (): number {
-    this.stocksQty -= this.currentOperation.qty
-    this.losses += (this.weightedAverage - this.currentOperation.unitCost) * this.currentOperation.qty
+  private sellWithLoss (operation: Operation): number {
+    this.stocksQty -= operation.qty
+    this.losses += (this.weightedAverage - operation.unitCost) * operation.qty
     return 0
   }
 
-  private sellWithinLimit (): number {
-    this.stocksQty -= this.currentOperation.qty
+  private sellWithinLimit (operation: Operation): number {
+    this.stocksQty -= operation.qty
     return 0
   }
 }
